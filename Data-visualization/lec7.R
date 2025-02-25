c2c135712128b5f57a546ba981c9c1553f632bdc
khalidkhankakar <khalidkhankakar2468@gmail.com>
2025-02-25 09:53:23 +0500

    add lecture 7

---
+++ Data-visualization/lec7.R
@@ -0,0 +1,23 @@
+y1= c(2,3,4,3,4,3,9,21,4,3)
+y2 =c(3,4,2,4,5,6,7,8,9,10)
+y3 = c(12,3,4,5,7,3,2,5,3,7)
+
+colors = c('blue', 'cyan','red', 'purple', 'yellow', 'green', 'orange', 'white', 'gray','brown')
+
+data = data.frame(y1,y2,y3)
+
+barplot(as.matrix(data), main = 'Component bar chart', ylab='Count',xlab='Colors', beside=FALSE, col=heat.colors(10))
+
+
+
+ #ASSIGNMENT:- show numeric labels on component bar chart and auto colors schem
+
+
+library(tidyverse)
+score=c(23,24,45,67,76,43,65,78,90,43,21)
+qplot(y=score, x="", geom="boxplot", col=I("red"), fill=I("lightblue"),
+      ylab="score", main="status score")
+
+data(chickwts)
+str(chickwts)
+# qplot(data=chickwts, x=feed, y=weight, )
