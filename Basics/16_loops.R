eda5e19658256d849e1c0579c88ce5f8a70802b5
khalidkhankakar <khalidkhankakar2468@gmail.com>
2025-02-26 23:26:43 +0500

    loops

---
+++ Basics/16_loops.R
@@ -0,0 +1,49 @@
+# For loop
+for (val in 1:5){
+  print(val)
+}
+
+alphabits = letters[1:26]
+for (letter in alphabits){
+  print(letter)
+}
+
+week <- c('Sunday',
+          'Monday',
+          'Tuesday',
+          'Wednesday',
+          'Thursday',
+          'Friday',
+          'Saturday')
+
+for (day in week)
+{
+  print(day)
+}
+
+my_list = list('a', TRUE, 'khalid', 12)
+for (l in my_list){
+  print(l)
+}
+
+
+my_matrix = matrix(1:9, nrow=3)
+
+for(i in seq_len(nrow(my_matrix))){
+  for (j in seq_len(ncol(my_matrix))){
+    current_element <- my_matrix[i, j]
+    print(paste("The current element is:", current_element))
+  }
+}
+
+
+my_dataframe <- data.frame(
+  Name = c("Joy", "Juliya", "Boby", "Marry"),
+  Age = c(40, 25, 19, 55),
+  Gender = c("M", "F", "M", "F")
+)
+
+for (i in seq_len(nrow(my_dataframe))){
+  print(my_dataframe[i,])
+}
+
