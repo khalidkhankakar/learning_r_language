1a7d33a710de0247658b90df57f710c0b150d160
khalidkhankakar <khalidkhankakar2468@gmail.com>
2025-03-04 09:43:46 +0500

    lec9 done

---
+++ Data-visualization/lec9.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+temprature = airquality$Temp
+hist(temprature)
+
+
+hist(temprature, 
+     main='Temprature against Frequency',
+     xlab = 'Temprature',
+     ylab='Frequency',
+     col = c('blue'),
+     xlim = c(50,100),
+     freq = TRUE
+     )
+
+
+hist(temprature, ylim=c(0,35))
+text(himids, hicounts, labels=hicounts, adj=c(0.5, -0.5))
+
+hist(temprature, ylim=c(0,35), breaks=20)
+
+hist(temprature, 
+     main='Temprature against Frequency',
+     xlab = 'Temprature',
+     ylab='Frequency',
+     col = c('blue'),
+     border='red',
+     xlim = c(50,100),
+     freq = TRUE,
+     breaks=c(55,60,70,75,80,100)
+     )
