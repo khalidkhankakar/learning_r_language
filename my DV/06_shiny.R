81d1395dd59f445db3ac601182110bddeff45379
khalidkhankakar <khalidkhankakar2468@gmail.com>
2025-03-05 22:51:46 +0500

    learn the basic of tidyr

--- my DV/06_shiny.R
+++ my DV/06_shiny.R
@@ -1,25 +1,61 @@
-# Load the Shiny package
 library(shiny)
+library(bslib)
 
-# Define UI (User Interface)
-ui <- fluidPage(
-  titlePanel("Shiny"),
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("num", "Choose a Number:", min = 1, max = 100, value = 50)
-    ),
-    mainPanel(
-      textOutput("result")
+# Define UI for app that draws a histogram ----
+ui <- page_sidebar(
+  # App title ----
+  title = "Hello Shiny!",
+  # Sidebar panel for inputs ----
+  sidebar = sidebar(
+    # Input: Slider for the number of bins ----
+    sliderInput(
+      inputId = "bins",
+      label = "Number of bins:",
+      min = 1,
+      max = 50,
+      value = 30
     )
-  )
+  ),
+  # Output: Histogram ----
+  plotOutput(outputId = "distPlot")
 )
 
-# Define Server Logic
+
+# Define server logic required to draw a histogram ----
 server <- function(input, output) {
-  output$result <- renderText({
-    paste("You selected:", input$num)
+  
+  # Histogram of the Old Faithful Geyser Data ----
+  # with requested number of bins
+  # This expression that generates a histogram is wrapped in a call
+  # to renderPlot to indicate that:
+  #
+  # 1. It is "reactive" and therefore should be automatically
+  #    re-executed when inputs (input$bins) change
+  # 2. Its output type is a plot
+  output$distPlot <- renderPlot({
+    
+    x    <- faithful$waiting
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    hist(x, breaks = bins, col = "#007bc2", border = "white",
+         xlab = "Waiting time to next eruption (in mins)",
+         main = "Histogram of waiting times")
+    
   })
+  
 }
 
-# Run the App
-shinyApp(ui = ui, server = server)
+runExample("01_hello")      # a histogram
+runExample("02_text")       # tables and data frames
+runExample("03_reactivity") # a reactive expression
+runExample("04_mpg")        # global variables
+runExample("05_sliders")    # slider bars
+runExample("06_tabsets")    # tabbed panels
+runExample("07_widgets")    # help text and submit buttons
+runExample("08_html")       # Shiny app built from HTML
+runExample("09_upload")     # file upload wizard
+runExample("10_download")   # file download wizard
+runExample("11_timer")      # an automated timer
+
+shinyApp(ui=ui, server=server)
+
---
+++ my DV/07_tidyr.R
@@ -0,0 +1,55 @@
+library(tidyr)
+# Example 1: Basic wide to long transformation
+wide_data <- data.frame(
+  name = c("Alice", "Bob"),
+  math_2022 = c(90, 85),
+  math_2023 = c(92, 88),
+  science_2022 = c(85, 80),
+  science_2023 = c(88, 82)
+)
+wide_data
+
+long_data <- wide_data %>% 
+  pivot_longer(
+    cols = -name,
+    names_to = c('.value', 'year'),
+    names_sep = '-'
+  )
+long_data
+
+
+long_example <- data.frame(
+  student = rep(c("Alice", "Bob"), each = 4),
+  subject = rep(c("math", "science"), times = 4),
+  year = rep(c(2022, 2023), each = 2, times = 2),
+  score = c(90, 85, 92, 88, 85, 80, 88, 82)
+)
+long_example
+
+
+wide_example = long_example %>% pivot_wider(
+  names_from = c(subject, year),
+  values_from = score
+)
+
+wide_example
+
+
+# replace the na values
+df_missing = data.frame(
+  x= c(1,3,NA),
+  y= c('khalid', NA, "khan")
+)
+df_missing
+
+df_replace_na = df_missing %>% replace_na(
+  list(x = 0, y = 'unknown')
+  
+)
+df_replace_na
+
+# Remove the na values in data.frame
+cleaned_df = df_missing %>% drop_na()
+cleaned_df
+
+
