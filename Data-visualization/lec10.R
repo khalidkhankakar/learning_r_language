97fa3104605362207f0944a55c688e12afdd2964
khalidkhankakar <khalidkhankakar2468@gmail.com>
2025-03-06 09:41:24 +0500

    10 done

--- Data-visualization/lec10.R
+++ Data-visualization/lec10.R
@@ -1,2 +1,11 @@
 home_data <- read.csv("https://raw.githubusercontent.com/rashida048/Datasets/master/home_data.csv")[, c('price', 'condition')]
 summary(home_data)
+hist(home_data$price)
+hist(home_data$price, breaks=20)
+hist(home_data$price, breaks=20, col='red')
+hist(home_data$price, breaks=20, col='red', freq=FALSE)
+hist(home_data$price, breaks=20, col='red', freq=FALSE, xlim=c(0, 2000000))
+# let's change the shape of the block
+hist(home_data$price, breaks=20, col='red', freq=FALSE, xlim=c(0, 2000000), border='blue', xlab='Price', ylab='Frequency')
+
+hist(home_data$condition)
