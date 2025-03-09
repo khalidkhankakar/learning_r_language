977f14f31c4dcb2ecee126c63b62adcca5ff21ff
khalidkhankakar <khalidkhankakar2468@gmail.com>
2025-03-09 19:40:32 +0500

    learn the grammar of graphics

---
+++ my DV/08_DV.R
@@ -0,0 +1,8 @@
+# The Grammar of Graphics
+# Data: The dataset you are visualizing.
+# Aesthetics: aes() Mapping variables to visual properties. such x-axies y-axies.
+# Geometrics: The type of plot
+# Facets: Small multiples (subplots by category).
+# Stats: Statistical transformations (e.g., binning, smoothing).
+# Coordinates: Axis scales and transformations.
+# Themes: Non-data ink (fonts, colors, backgrounds).
