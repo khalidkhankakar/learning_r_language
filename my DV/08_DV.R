31c68167cd538c5c2076a3faaab1634c6be2447a
khalidkhankakar <khalidkhankakar2468@gmail.com>
2025-03-09 19:58:57 +0500

    learn the scatter plot

--- my DV/08_DV.R
+++ my DV/08_DV.R
@@ -6,3 +6,47 @@
 # Stats: Statistical transformations (e.g., binning, smoothing).
 # Coordinates: Axis scales and transformations.
 # Themes: Non-data ink (fonts, colors, backgrounds).
+
+
+# Scatter plot
+# Scatter plot is used to visualize the relationship between two variables.
+library(ggplot2)
+data = data.frame(
+  x = rnorm(100, mean=50, sd=10),
+  y = rnorm(100, mean=60, sd=5)
+)
+
+data
+
+# Method 1: by using the built-in plot function
+plot(data$x, data$y, main = 'Scatter Plot X vs Y', xlab = 'X axies', ylab = 'Y axies', pch=19, col='red')
+
+
+# Method 2: by using the ggplot2 library
+ggplot(data, aes(x=x, y=x)) +
+  geom_point(color='blue') +
+  geom_smooth(method = 'lm', se=FALSE,color='red') +
+  ggtitle('Scatter plot with Regression line') +
+  xlab('X axies') +
+  ylab('Y axies')
+  theme_minimal()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
