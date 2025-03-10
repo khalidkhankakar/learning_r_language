2926049ec2077124475f5dd89f00d6beb463797f
khalidkhankakar <khalidkhankakar2468@gmail.com>
2025-03-10 12:27:51 +0500

    lecture 11 done

--- Data-visualization/lec10.R
+++ Data-visualization/lec10.R
@@ -1,7 +1,7 @@
 home_data <- read.csv("https://raw.githubusercontent.com/rashida048/Datasets/master/home_data.csv")[, c('price', 'condition')]
 summary(home_data)
 hist(home_data$price)
-hist(home_data$price, breaks=20)
+hist(home_data$price, probability = TRUE,breaks='scott')
 hist(home_data$price, breaks=20, col='red')
 hist(home_data$price, breaks=20, col='red', freq=FALSE)
 hist(home_data$price, breaks=20, col='red', freq=FALSE, xlim=c(0, 2000000))
@@ -9,3 +9,6 @@ hist(home_data$price, breaks=20, col='red', freq=FALSE, xlim=c(0, 2000000))
 hist(home_data$price, breaks=20, col='red', freq=FALSE, xlim=c(0, 2000000), border='blue', xlab='Price', ylab='Frequency')
 
 hist(home_data$condition)
+
+# take mtcars data make the histogram
+
---
+++ Data-visualization/lec11.R
@@ -0,0 +1,48 @@
+summary(mtcars)
+
+x = mtcars$mpg
+
+h = hist(mtcars$mpg,col = 'purple')
+xfit = seq(min(x), max(x), length= 40)
+xfit
+yfit = dnorm(xfit, mean= mean(x), sd= sd(xfit))
+yfit
+
+yfit= yfit * diff(h$mids[1:2]) * length(x)
+
+lines(xfit, yfit, col='yellow', lwd=3)
+
+
+d = density(mtcars$mpg)
+plot(d, main='kernel density')
+
+polygon(d, col='blue', border='yellow')
+
+
+library(ggplot2)
+library(dplyr)
+# library(hrbrthemes)
+
+data = data.frame(
+  type=c(rep('variable1', 1000), rep('variable2', 1000)),
+  values = c(rnorm(1000), rnorm(1000,mean=4))
+)
+data
+data[1700:2000, ]
+
+n = data %>% 
+  ggplot(aes(x=values, fill = type)) +
+  geom_histogram(color='gray', alpha=0.6, position='identity')+
+  scale_fill_manual(values = c('green', 'blue')) +
+  theme_classic()+
+  labs(fill='')
+n  
+  
+attach(mtcars)
+par(mfrow=c(1,2))
+hist(mtcars$mpg)
+hist(mtcars$mpg,prob=1)
+
+
+
+
