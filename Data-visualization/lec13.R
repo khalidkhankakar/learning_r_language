20b7c142494cbfc1a64901905edcc3b5ee0d6afd
khalidkhankakar <khalidkhankakar2468@gmail.com>
2025-03-13 09:39:45 +0500

    lec 13 done

---
+++ Data-visualization/lec13.R
@@ -0,0 +1,23 @@
+# structure of the data which is formed from the binomial distribution
+
+plot_bionomial = function(n, p){
+ x= 0:n
+ bio = dbinom(x, n, p)
+ mu_x = n*p
+ plot(x, bio, type="h",
+      lwd=2,col="blue",
+      xlab="x",ylab="f(x)",
+      abline(v=mu_x, col="red", lwd=2, lty=2),
+      main=paste("Density = ",n,' P=',p))
+}
+
+
+n=50
+values = c(0.05, 0.1, 0.2, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9)
+par(nfrow=c(2,3))
+
+for(p in values){
+ plot_bionomial(n, p)
+}
+
+
