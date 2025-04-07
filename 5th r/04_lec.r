fbc6189c25106d79717058d1d65befa51d1737b7
khalidkhankakar <khalidkhankakar2468@gmail.com>
2025-04-07 23:07:01 +0500

    boxplot in tidyverse

--- 5th r/04_lec.r
+++ 5th r/04_lec.r
@@ -1,3 +1,12 @@
 # Box plot
 library(tidyverse)
 score = c(12,23,45,67,78,56,42,23)
+qplot(
+    x = score,
+    geom = "boxplot",
+    main = "Box Plot",
+    xlab = "Score",
+    ylab = "Frequency",
+    fill = I("blue"),
+    col = I("red")
+)
